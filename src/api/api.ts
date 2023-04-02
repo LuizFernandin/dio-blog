bdae2b5f9c512e5eaa8e8f3085617e93fbd84d63
LuizFernandin <luizx2012x@gmail.com>
2023-04-02 16:09:52 -0300

    Finalizado

---
+++ src/api/api.ts
@@ -0,0 +1,3 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient("https://oxswzwkaifohdtpqmzpn.supabase.co/", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94c3d6d2thaWZvaGR0cHFtenBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAzOTcxMjIsImV4cCI6MTk5NTk3MzEyMn0.17d46qEiataGG618ZJqM_JOQdr98zNSO0JY9YPrHjjI");
